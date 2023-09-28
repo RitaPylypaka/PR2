d65cff0648fa5c57c4c579893f33b711d6b99f60
RitaPylypaka <ritapylypaka@gmail.com>
2023-09-28 13:54:22 +0300

    com

---
+++ PR2/PR2.cpp
@@ -0,0 +1,23 @@
+// Lab_02.1.cpp
+// < Пилипака Маргарита >
+// Лабораторна № 2
+// Лінійні програми.
+// Варіант 20
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double m; //вихідний параметр
+	double n; //вихідний параметр
+	double z1; //результат обчислення 1-го виразу
+	double z2; //результат обчислення 2-го виразу
+	cout << "m = "; cin >> m;
+	cout << "n = "; cin >> n;
+	z1 = ((m - 1) * (sqrt(m)) - (n - 1) * (sqrt(n))) / ((sqrt(m * m * m * n)) + (m * n + m * m) - m);
+	z2 = ((sqrt(m)) - (sqrt(n))) / m;
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
